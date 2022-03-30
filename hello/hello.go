62482418118e5dce77ac2a943d0aaf642162045b
OoiBP <bpooi0702@gmail.com>
2022-03-30 20:58:35 +0800

    Improve hello.go with language

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	} else if language == french {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
